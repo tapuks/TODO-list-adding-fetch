ea5ea37e49d7f8ed9794eb859044ee1f6666fa64
tapuks <david_berdiell@hotmail.com>
2021-05-20 19:08:29 +0000

    edit Styles

--- src/js/component/ToDoList.js
+++ src/js/component/ToDoList.js
@@ -5,34 +5,35 @@ const ToDoList = () => {
 	const [todos, setTodos] = useState([]);
 	const [task, setTask] = useState("");
 
-	fetch("https://assets.breatheco.de/apis/fake/todos/user/david", {
-		method: "PUT",
-		body: JSON.stringify(todos),
-		headers: {
-			"Content-Type": "application/json"
-		}
-	})
-		.then(resp => {
-			console.log(resp.ok); // Será true (verdad) si la respuesta es exitosa.
-			console.log(resp.status); // el código de estado = 200 o código = 400 etc.
-			console.log(resp.text()); // Intentará devolver el resultado exacto como cadena (string)
-			return resp.json(); // (regresa una promesa) will try to parse the result as json as return a promise that you can .then for results
-		})
-		.then(data => {
-			//Aquí es donde debe comenzar tu código después de que finalice la búsqueda
-			console.log(data); //esto imprimirá en la consola el objeto exacto recibido del servidor
-		})
-		.catch(error => {
-			//manejo de errores
-			console.log(error);
-		});
-
+	// fetch("https://assets.breatheco.de/apis/fake/todos/user/david", {
+	// 	method: "PUT",
+	// 	body: JSON.stringify(todos),
+	// 	headers: {
+	// 		"Content-Type": "application/json"
+	// 	}
+	// })
+	// 	.then(resp => {
+	// 		console.log(resp.ok); // Será true (verdad) si la respuesta es exitosa.
+	// 		console.log(resp.status); // el código de estado = 200 o código = 400 etc.
+	// 		console.log(resp.text()); // Intentará devolver el resultado exacto como cadena (string)
+	// 		return resp.json(); // (regresa una promesa) will try to parse the result as json as return a promise that you can .then for results
+	// 	})
+	// 	.then(data => {
+	// 		//Aquí es donde debe comenzar tu código después de que finalice la búsqueda
+	// 		console.log(data); //esto imprimirá en la consola el objeto exacto recibido del servidor
+	// 	})
+	// 	.catch(error => {
+	// 		//manejo de errores
+	// 		console.log(error);
+	// 	});
+	let counter = todos.length;
 	const addTask = () => {
-		if (task != "") {
+		if (task != "" && event.key === "Enter") {
 			const newTodos = todos.concat({
 				title: task,
 				id: todos.length + 1
 			});
+			console.log(counter);
 			setTodos(newTodos);
 		}
 	};
@@ -52,10 +53,8 @@ const ToDoList = () => {
 						value={task}
 						placeholder="Whats needs to be done"
 						onChange={e => setTask(e.target.value)}
+						onKeyDown={addTask}
 					/>
-					<button className="btn btn-primary" onClick={addTask}>
-						Add
-					</button>
 				</div>
 
 				<div className="contain-ul">
@@ -69,7 +68,7 @@ const ToDoList = () => {
 									<span
 										className="button-delete"
 										onClick={() => deleteTask(todo.id)}>
-										<i class="fas fa-times button-delete"></i>
+										<i className="fas fa-times button-delete"></i>
 									</span>
 								</li>
 							);
