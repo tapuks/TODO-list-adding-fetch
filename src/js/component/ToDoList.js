9a29f38ab7d507aaf3ec131307d3d3792022b6fc
tapuks <david_berdiell@hotmail.com>
2021-10-15 11:07:24 +0000

    finish

--- src/js/component/ToDoList.js
+++ src/js/component/ToDoList.js
@@ -1,82 +1,96 @@
+import { event } from "jquery";
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const ToDoList = () => {
-	const [todos, setTodos] = useState([]);
 	const [task, setTask] = useState("");
+	const [todos, setTodos] = useState([]);
 
-	// fetch("https://assets.breatheco.de/apis/fake/todos/user/david", {
-	// 	method: "PUT",
-	// 	body: JSON.stringify(todos),
-	// 	headers: {
-	// 		"Content-Type": "application/json"
-	// 	}
-	// })
-	// 	.then(resp => {
-	// 		console.log(resp.ok); // Será true (verdad) si la respuesta es exitosa.
-	// 		console.log(resp.status); // el código de estado = 200 o código = 400 etc.
-	// 		console.log(resp.text()); // Intentará devolver el resultado exacto como cadena (string)
-	// 		return resp.json(); // (regresa una promesa) will try to parse the result as json as return a promise that you can .then for results
-	// 	})
-	// 	.then(data => {
-	// 		//Aquí es donde debe comenzar tu código después de que finalice la búsqueda
-	// 		console.log(data); //esto imprimirá en la consola el objeto exacto recibido del servidor
-	// 	})
-	// 	.catch(error => {
-	// 		//manejo de errores
-	// 		console.log(error);
-	// 	});
-	let counter = todos.length;
-	const addTask = () => {
-		if (task != "" && event.key === "Enter") {
+	const addTask = e => {
+		if (task != "" && e.key === "Enter") {
+			console.log("si");
 			const newTodos = todos.concat({
-				title: task,
-				id: todos.length + 1
+				label: task,
+				done: false
 			});
-			console.log(counter);
+			// console.log(counter);
 			setTodos(newTodos);
+			const options = {
+				method: "PUT",
+				headers: {
+					"Content-Type": "application/json"
+				},
+				body: JSON.stringify(todos)
+			};
+
+			fetch(
+				"https://assets.breatheco.de/apis/fake/todos/user/david",
+				options
+			)
+				.then(res => console.log(res))
+				.then(json => console.log(json))
+				.catch(error => console.log(error));
+			console.log(todos);
 		}
 	};
-	const deleteTask = todoId => {
-		console.log(todoId);
-		const newTodos2 = todos.filter(todo => todo.id !== todoId);
+
+	const deleteTask = todoLabel => {
+		// console.log(todoId);
+		const newTodos2 = todos.filter(todo => todo.label !== todoLabel);
 		setTodos(newTodos2);
+
+		const options = {
+			method: "PUT",
+			headers: {
+				"Content-Type": "application/json"
+			},
+			body: JSON.stringify(todos)
+		};
+
+		fetch("https://assets.breatheco.de/apis/fake/todos/user/david", options)
+			.then(res => console.log(res))
+			.then(json => console.log(json))
+			.catch(error => console.log(error));
+		console.log(todos);
 	};
 
+	useEffect(() => {
+		fetch("https://assets.breatheco.de/apis/fake/todos/user/david")
+			.then(res => res.json())
+			.then(json => setTodos(json))
+			.catch(error => console.log("error"));
+	}, []);
+	console.log("todos", todos);
+
 	return (
-		<div>
-			<div className="contain">
-				<h1 className="title">todos</h1>
-				<div className="contain-input">
+		<>
+			<h1>Todo List</h1>
+			<div className="container">
+				<div className="div-input">
 					<input
 						type="text"
-						value={task}
 						placeholder="Whats needs to be done"
 						onChange={e => setTask(e.target.value)}
 						onKeyDown={addTask}
 					/>
 				</div>
-
-				<div className="contain-ul">
-					<ul>
-						{todos.map(todo => {
-							return (
-								<li key={todo.id}>
-									<span className="li-text">
-										{todo.title}
-									</span>
-									<span
-										className="button-delete"
-										onClick={() => deleteTask(todo.id)}>
-										<i className="fas fa-times button-delete"></i>
-									</span>
-								</li>
-							);
-						})}
-					</ul>
+				<div className="div-todos">
+					{todos.map(todo => {
+						return (
+							<li key={todo.length + 1}>
+								<span className="li-text">{todo.label}</span>
+								<span
+									className="button-delete"
+									onClick={() => deleteTask(todo.label)}>
+									<i className="fas fa-times button-delete"></i>
+								</span>
+							</li>
+						);
+					})}
 				</div>
+				<span>Total:{todos.length}</span>
 			</div>
-		</div>
+		</>
 	);
 };
 
